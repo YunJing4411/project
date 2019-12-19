949691efbbde385895b34e6df433cfb49f4921ad
ShaoJyun <d0683320@mail.fcu.edu.tw>
2019-12-20 00:57:23 +0800

    update signup

--- app/src/main/java/com/example/project/Signup.java
+++ app/src/main/java/com/example/project/Signup.java
@@ -1,14 +1,73 @@
 package com.example.project;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import android.os.Bundle;
 
 public class Signup extends AppCompatActivity {
 
+    EditText name,mail,password;
+    Button SignUp;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_main);
+
+        name = (EditText)findViewById(R.id.Name);
+        mail = (EditText)findViewById(R.id.Email);
+        password = (EditText)findViewById(R.id.Password);
+        SignUp = (Button)findViewById(R.id.SignUp);
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+        SignUp.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                final ProgressDialog mDialog = new ProgressDialog(Signup.this);
+                mDialog.setMessage("Please waiting....");
+                mDialog.show();
+
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.child(name.getText().toString()).exists()){
+                            mDialog.dismiss();
+                            Toast.makeText(Signup.this, "Name already exist !", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            mDialog.dismiss();
+                            User user = new User(name.getText().toString(), mail.getText().toString(), password.getText().toString());
+                            table_user.child(name.getText().toString()).setValue(user);
+                            Toast.makeText(Signup.this, "Sign up successful !", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/project/User.java
@@ -0,0 +1,41 @@
+package com.example.project;
+
+public class User {
+
+    private String name;
+    private String mail;
+    private String password;
+
+
+    public User() {
+
+    }
+    public User(String name, String mail, String password) {
+        this.name = name;
+        this.mail = mail;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
